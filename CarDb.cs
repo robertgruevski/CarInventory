5c391d87791fefd65e207b978991581b92ead525
robertgruevski <robertgruevski@outlook.com>
2025-01-21 13:10:08 -0500

    Define Database. Create table for Database. Create Database in memory.

---
+++ CarDb.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace CarInventory
+{
+	public class CarDb : DbContext // Db needs to inherit from DbContext
+	{
+		public CarDb(DbContextOptions<CarDb> options) : base(options) { } // Takes in options to pass to DbContext
+		public DbSet<Car> Cars { get; set; } // Creates the Cars table in the Db
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,13 @@
+using CarInventory;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Dependency Injection -- Will construct a CarDb object when needed
+// Will Allow the CarDb to be injected into the methods that require it
+builder.Services.AddDbContext<CarDb>(opt => opt.UseInMemoryDatabase("CarList"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
