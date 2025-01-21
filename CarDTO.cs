b77bf122a738cf9207106f02e0cc17571b3bdf71
robertgruevski <robertgruevski@outlook.com>
2025-01-21 13:53:19 -0500

    Crud implementation

---
+++ CarDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace CarInventory
+{
+	public class CarDTO
+	{
+		public int Id { get; set; } // the Primary Key
+		public string Make { get; set; }
+		public string Model { get; set; }
+		public bool IsAvailable { get; set; }
+		public CarDTO() { }
+		public CarDTO(Car car)
+		{
+			Id = car.Id;
+			Make = car.Make;
+			Model = car.Model;
+			IsAvailable = car.IsAvailable;
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,48 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+RouteGroupBuilder cars = app.MapGroup("/cars"); // All of the endpoints begin with /cars
+
+cars.MapGet("/", GetAllCars);
+cars.MapPost("/", CreateCar);
+cars.MapGet("/{id}", GetCar);
+cars.MapDelete("/{id}", DeleteCar);
 
 app.Run();
+
+static async Task<IResult> GetAllCars(CarDb db)
+{
+	return TypedResults.Ok(await db.Cars.Select(x => new CarDTO(x)).ToArrayAsync());
+}
+
+static async Task<IResult> CreateCar(Car car, CarDb db)
+{
+	db.Cars.Add(car);
+	await db.SaveChangesAsync();
+
+	var dto = new CarDTO(car);
+
+	return TypedResults.Created($"/cars/{dto.Id}", dto);
+}
+
+static async Task<IResult> GetCar(int id, CarDb db)
+{
+	return await db.Cars.FindAsync(id)
+		is Car car
+			? TypedResults.Ok(new CarDTO(car))
+			: TypedResults.NotFound();
+}
+
+static async Task<IResult> DeleteCar(int id, CarDb db)
+{
+	if (await db.Cars.FindAsync(id) is Car car)
+	{
+		db.Cars.Remove(car);
+		await db.SaveChangesAsync();
+		return TypedResults.NoContent();
+	}
+	else
+	{
+		return TypedResults.NotFound();
+	}
+}
