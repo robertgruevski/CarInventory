42d57c9cd850731babc890b052d83da7ef281ee7
robertgruevski <robertgruevski@outlook.com>
2025-01-21 13:01:30 -0500

    Car Class with properties

---
+++ Car.cs
@@ -0,0 +1,11 @@
+﻿namespace CarInventory
+{
+	public class Car
+	{
+		public int Id { get; set; } // the Primary Key
+		public string Make { get; set; }
+		public string Model { get; set; }
+		public bool IsAvailable { get; set; }
+		public string? Secret { get; set; }
+	}
+}
